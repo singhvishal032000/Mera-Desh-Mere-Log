abb780883abe63dce4f72cd22fb00e4831ba3097
Vishal Kumar <singhvishal032000@gmail.com>
2025-06-22 12:27:42 +0530

    Update constant.js

--- fronted/src/utils/constant.js
+++ fronted/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:3000/user"
-export const JOB_API_END_POINT="http://localhost:3000/job"
-export const APPLICATION_API_END_POINT="http://localhost:3000/application"
-export const COMPANY_API_END_POINT="http://localhost:3000/company"
+export const USER_API_END_POINT="https://mera-desh-mere-log-backend.onrender.com/user"
+export const JOB_API_END_POINT="https://mera-desh-mere-log-backend.onrender.com/job"
+export const APPLICATION_API_END_POINT="https://mera-desh-mere-log-backend.onrender.com/application"
+export const COMPANY_API_END_POINT="https://mera-desh-mere-log-backend.onrender.com/company"
